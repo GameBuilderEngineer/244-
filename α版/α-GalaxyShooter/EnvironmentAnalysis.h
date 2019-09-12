14e16bc4f204824223cd1dfb7de45656bdd46b16
中込和輝 <astora.home@gmail.com>
2019-09-12 20:30:57 +0900

    AIの移動到着判定距離修正

--- α版/α-GalaxyShooter/EnvironmentAnalysis.h
+++ α版/α-GalaxyShooter/EnvironmentAnalysis.h
@@ -32,7 +32,7 @@ namespace EnvilonmentAnalysisNs
 	const int	NUM_TEST_CASES_PER_NODE = NUM_TEST_SIZE * NUM_TEST_ZONE;// 一つのノードについて衝突検知の試行回数
 
 	// リカージョンエリア判定用バウンディングスフィア
-	const float	TEST_SIZE_RADIUS_BASE = 13.0f;			// 基本半径
+	const float	TEST_SIZE_RADIUS_BASE = 14.0f;			// 基本半径
 	const float	TEST_SIZE_RADIUS_ADD = 5.0f;			// 追加半径
 	const float  TEST_SIZE_RADIUS_FUZZY_ADJUST = 1.0f;	// サイズ集合が各1.0のときに含む実際のBS半径の最小最大との差
 	const float  AXIS_TILT_ANGLE_FOR_SMALL_BS = 0.22f;	// サイズに応じた配置半径を作るためレイを傾ける角度
@@ -40,7 +40,7 @@ namespace EnvilonmentAnalysisNs
 	const float  AXIS_TILT_ANGLE_FOR_BIG_BS = 0.42f;	// サイズに応じた配置半径を作るためレイを傾ける角度
 
 	// 相手をリカージョンする際の半径
-	const float  SIZE_RADIUS_OPPONENT_RECURSION = 11.5f;
+	const float  SIZE_RADIUS_OPPONENT_RECURSION = 12.0f;
 
 	// ※ファジー入力値算出にあたっての定数は直接selectRecursionArea()をいじる方が早いのでここには記載しない
 }
